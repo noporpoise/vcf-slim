7cfe1a2dbdd0503adb056726a0d71ac7aa7f6a89
Isaac Turner <turner.isaac@gmail.com>
2015-12-19 18:36:17 +0000

    script: vcf line add missing column

--- scripts/sam-name-to-vcf.sh
+++ scripts/sam-name-to-vcf.sh
@@ -6,6 +6,7 @@ DATE=$(date '+%Y%m%d')
 
 echo '##fileformat=VCFv4.1
 ##fileDate='"$DATE"'
+##FILTER=<ID=PASS,Description="All filters passed">
 ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">'
 
 echo '#CHROM POS ID REF ALT QUAL FILTER INFO FORMAT' | tr ' ' "\t"
@@ -13,4 +14,4 @@ echo '#CHROM POS ID REF ALT QUAL FILTER INFO FORMAT' | tr ' ' "\t"
 # Input from vcf-slim/bin/vcfcontigs:
 # NC_009648.1:813:.:C:T:1:pos:50:ltrim:0:rtrim:0
 gzip -fcd $@ | tr ':' '\t' | cut -f 1-5 | \
-  while read line; do printf "$line\t.\t.\tGT\n"; done
+  while read line; do printf "$line\t.\t.\t.\tGT\n"; done
